2ac8f11ae99c5563f0013878c2d1caefea851f6a
SporeSun <135516150+SporeSun@users.noreply.github.com>
2023-11-20 20:38:22 -0800

    altered database and editor files to correctly execute

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,26 +11,42 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+  /**
+ * Adds or updates content in the database.
+ * @param {string} content The content to be stored.
+ */
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add('content');
+
+  // Use a fixed ID for your entry, e.g., 1
+  const request = store.put({ id: 1, content });
   const result = await request;
-  console.log('Data saved to the database', result);
+  console.log('🚀 - data saved to the database', result);
+
+  // Complete the transaction
+  await tx.done;
+  return result;
 };
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+  console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+
+  // Retrieve the entry with the fixed ID, e.g., 1
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.content;
 };
+
 
 initdb();
